8e6bf48635e704a6f129a515c327fe4cfc7b2e52
Your Name <you@example.com>
2024-03-27 00:30:46 +0300

    xz

--- main/views.py
+++ main/views.py
@@ -1,9 +1,21 @@
 from django.shortcuts import render
-from .models import Product
+from .models import Product, Category
 
 def index(request):
     return render(request, 'main/index.html')
 
 def product_list(request):
     products = Product.objects.all()
-    return render(request, 'main/product_list.html', {'products': products})
+    categories = Category.objects.all()
+
+    # Фильтрация по категории
+    category = request.GET.get('category')
+    if category:
+        products = products.filter(category__name=category)
+
+    # Сортировка
+    sort_by = request.GET.get('sort_by')
+    if sort_by:
+        products = products.order_by(sort_by)
+
+    return render(request, 'main/product_list.html', {'products': products, 'categories': categories})
