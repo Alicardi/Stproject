dc3f6af65313f7c59625d0f2771d233bdd1ff0d7
Your Name <you@example.com>
2024-04-06 02:33:57 +0300

    Модальное окно в продуктах (надо доделать) + upd переходы по ссылкам

--- main/models.py
+++ main/models.py
@@ -7,12 +7,17 @@ class Category(models.Model):
         return self.name
 
 class Product(models.Model):
-    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True) # Использовать как бренд
     name = models.CharField(max_length=100)
     description = models.TextField()
+
+    volume = models.CharField(max_length=100, blank=True)
+    properties = models.JSONField(blank=True, null=True)  
+    technologies = models.JSONField(blank=True, null=True)  
+
     price = models.DecimalField(max_digits=10, decimal_places=2)
     image = models.ImageField(upload_to='media/products/')
-
+    
     def __str__(self):
         return self.name
 
