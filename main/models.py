3b81dd71f615666ea689f41ec8f246d1fc54758b
admin <hollymolly6@mail.ru>
2024-06-06 18:11:51 +0300

    Вторая картинка в продуктах, но с линией и пробелом

--- main/models.py
+++ main/models.py
@@ -9,24 +9,19 @@ class Category(models.Model):
 class Product(models.Model):
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True) # Использовать как бренд
     name = models.CharField(max_length=100)
-    description = models.TextField()
 
     volume = models.CharField(max_length=100, blank=True)
-    properties = models.JSONField(blank=True, null=True)  
-    technologies = models.JSONField(blank=True, null=True)  
 
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    image = models.ImageField(upload_to='media/products/')
+    image = models.ImageField(upload_to='products/')
+    secondary_image = models.ImageField(upload_to='products/', blank=True, null=True)
     
     def __str__(self):
         return self.name
 
 class ProductImage(models.Model):
-    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='images')
-    image = models.ImageField(upload_to='media/products/')
-
-    def __str__(self):
-        return f"Image for {self.product.name}"
+    product = models.ForeignKey(Product, related_name='images', on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='products/')
     
 class GalleryImage(models.Model):
     image = models.ImageField(upload_to='gallery_images/')
